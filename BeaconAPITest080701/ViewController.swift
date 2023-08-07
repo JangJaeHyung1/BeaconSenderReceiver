8d03e3a08315f752f87deea33ea644b780f8a9a3
jangjaehyung1 <drezzed7@gmail.com>
2023-08-07 10:09:45 +0900

    Initial Commit

---
+++ BeaconAPITest080701/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeaconAPITest080701
+//
+//  Created by jh on 2023/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
